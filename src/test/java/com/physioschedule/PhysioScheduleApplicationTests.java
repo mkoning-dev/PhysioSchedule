15432c356f4f9f37bc70be8c94b91a66c6a92763
Martijn Koning <mkoningbb@gmail.com>
2024-11-06 14:14:09 -0600

    Initial commit - Spring Boot project setup

---
+++ src/test/java/com/physioschedule/PhysioScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.physioschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysioScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
